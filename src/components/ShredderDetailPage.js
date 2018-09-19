fe156b5c3a4dec64fe091aba6b272c09458cdb2d
Sean MacDonald <seandoylemacdonald@gmail.com>
2018-09-18 23:05:09 -0700

    added readme with project overview and added styling to ShredderDetailPage

--- src/components/ShredderDetailPage.js
+++ src/components/ShredderDetailPage.js
@@ -1,16 +1,31 @@
 import React, { Component } from 'react'; 
-import { Text } from 'react-native'; 
+import { Text, View } from 'react-native'; 
 
 class ShredderDetailPage extends Component {
+    static navigationOptions = {
+        headerStyle: {
+                backgroundColor: '#dd7ac8'
+        }
+    };
+
     render() {
         const uid = this.props.navigation.getParam('uid');
         console.log(`SHREDDER DETAILS FOR: ${uid}`);
         return (
-            <Text>
-                TODO: Implement ShredderDetailPage
-            </Text>
+            <View style={styles.containerStyle}>
+                <Text>
+                    TODO: Implement ShredderDetailPage
+                </Text>
+            </View>
         );
     }
 }
+
+const styles = {
+    containerStyle: {
+        backgroundColor: 'white', 
+        flex: 1
+    }
+}; 
 
 export default ShredderDetailPage;
